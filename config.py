2a36a3e37d3e0190f62762d6b0d5ec732772c5c7
Artorias-23 <137784746+Artorias-23@users.noreply.github.com>
2025-02-12 13:42:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-TWILIO_ACCOUNT_SID = "AC8edce726fa736c4d7441797a7417d400"
-TWILIO_AUTH_TOKEN = "0c02ce7f05daa64d5e896b141485f1dc"
+TWILIO_ACCOUNT_SID = ""
+TWILIO_AUTH_TOKEN = ""
 TWILIO_PHONE_NUMBER = "+14198714333"  # Your Twilio phone number
-RECIPIENT_PHONE_NUMBER = "+918855985619"  # Recipient's phone number
+RECIPIENT_PHONE_NUMBER = ""  # Recipient's phone number
 MeSSage = "!!! ALERT !!! Unsafe Gesture Detected. Someone is in danger in room 503."
